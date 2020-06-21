158052e75e855df98f1e566d351097e461a2e42c
paulohz <pauloh.alvesdacosta@gmail.com>
2020-06-20 23:02:01 -0300

    create user in database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ServicoPrestadoService } from './servico-prestado.service';
 import { LoginComponent } from './login/login.component'
 import { FormsModule } from '@angular/forms';
 import { LayoutComponent } from './layout/layout.component'
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { LayoutComponent } from './layout/layout.component'
   ],
   providers: [
     ClientesService,
-    ServicoPrestadoService
+    ServicoPrestadoService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
